d1b1a16750ea7956ecf59de5a6c85d6227390a38
bibhutiswain9 <bibhutiswain990@gmail.com>
2024-03-24 01:27:16 +0530

    add expo router app

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Index() {
+  return (
+    <View className="bg-cyan-500">
+      <Text className="text-7xl">Index</Text>
+    </View>
+  );
+}
